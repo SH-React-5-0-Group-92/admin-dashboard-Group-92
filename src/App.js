9a7c7bb35ac2a0e16ad7bcb71d5d27bd6699bc80
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-16 23:21:11 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<ProtectedRoutes />} />
+          <Route path="/*" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
