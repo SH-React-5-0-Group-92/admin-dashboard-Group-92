8badfeca19a8b5d3fdb19415218d645a2d5b1094
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-16 23:19:33 +0900

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,7 @@
+import { Routes, Route, Navigate, UseLocation, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Clients from "../components/Dashboard/Clients";
+
+export default function Dashboard() {
+  return <div>DAshboard</div>;
+}
