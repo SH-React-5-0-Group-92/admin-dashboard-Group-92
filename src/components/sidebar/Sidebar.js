b83f13e65b29715b5dc70c9034955fc2de86929b
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-19 00:04:35 +0900

    Create sidebar.js and scss files

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Group92Admin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li>
+            <span> Dashboard</span>
+          </li>
+          <li>
+            <span> Dashboard</span>
+          </li>
+          <li>
+            <span> Dashboard</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
