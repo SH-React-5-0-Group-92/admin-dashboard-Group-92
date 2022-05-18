c3afaf648044487521c0f521f2c75158271339ea
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-19 00:47:11 +0900

    Update Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,11 @@
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const Sidebar = () => {
   return (
@@ -9,14 +16,37 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAin</p>
           <li>
+            <DashboardIcon className="icon" />
             <span> Dashboard</span>
           </li>
           <li>
-            <span> Dashboard</span>
+            <AccountCircleIcon className="icon" />
+            <span> Users</span>
           </li>
           <li>
-            <span> Dashboard</span>
+            <ProductionQuantityLimitsIcon className="icon" />
+            <span> Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon" />
+            <span> Orders</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span> Settings</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span> Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span> Logout</span>
+          </li>
+          <li>
+            <span> color options</span>
           </li>
         </ul>
       </div>
