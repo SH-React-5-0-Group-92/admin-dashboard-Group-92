3e61ca41395430c70c9f6c8b0ac039290b2ae6d5
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-19 02:13:58 +0900

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,30 @@
 import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <SettingsOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img alt="" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
