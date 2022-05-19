69603d8bb56fdc91996581c423763157ab7195aa
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-19 22:01:32 +0900

    Navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,7 +19,11 @@ const Navbar = () => {
             <SettingsOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            <img alt="" className="avatar" />
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
           </div>
         </div>
       </div>
