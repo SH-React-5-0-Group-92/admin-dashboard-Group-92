3bb5ac22594cdc29be2241f274405c971dfbcbbb
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-21 14:51:10 +0900

    Featured component updated

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -3,6 +3,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
   return (
@@ -20,6 +22,29 @@ const Featured = () => {
         <p className="desc">
           Processing previous transaction. Last payments may not be included.
         </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle"> Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount"> $11.1k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle"> Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount"> $11.1k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle"> Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount"> $11.1k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
