1c2a10aed67d69c84af0388cb67948a831032b24
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-21 10:19:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+//import Login from "./pages/login/Login";
 import Clients from "./pages/users/Clients";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import Signup from "./components/Auth/Signup";
+import Sidebar from "./components/sidebar/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -12,12 +16,16 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
             <Route path="users">
               <Route path="client" element={<Clients />} />
             </Route>
             <Route path="new" element={<New />} />
             <Route path=":userId" element={<Single />} />
+
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/*" element={<Sidebar />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
