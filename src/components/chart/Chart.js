25263aa5ada179c2a4295692fb29da36021dc754
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-05-22 20:39:03 +0900

    Update Chart.js

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -12,29 +12,46 @@ import "./chart.scss";
 
 const data = [
   {
-    name: "January",
-    Total: 1400,
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
   },
   {
-    name: "February",
-    Total: 3000,
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
   },
   {
-    name: "March",
-    Total: 2000,
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
   },
   {
-    name: "April",
-    Total: 2780,
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
   },
   {
-    name: "May",
-    Total: 1890,
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
   },
   {
-    name: "June",
-
-    Total: 3800,
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
   },
 ];
 
